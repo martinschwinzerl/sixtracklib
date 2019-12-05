b6be28cefca5cc469f13d326d73a026279326656
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2019-12-06 00:36:56 +0100

    common: updates generated/config.h to conform to more advanced feature selection

---
+++ cmake/SetupSettingsCacheProperties.cmake
@@ -0,0 +1,54 @@
+if(  NOT SETUP_CACHE_PROPERTIES_FINISHED )
+    set( SETUP_CACHE_PROPERTIES_FINISHED 1 )
+
+    # ==========================================================================
+    # SixTrackLib Features :
+
+    set_property( CACHE SIXTRACKL_ENABLE_BEAM_FIELDS_4D_MAP
+        PROPERTY HELPSTRING "Enable tracking beam-beam-4D map" )
+
+    set_property( CACHE SIXTRACKL_ENABLE_BEAM_FIELDS_6D_MAP
+        PROPERTY HELPSTRING "Enable tracking beam-beam-6D map" )
+
+    set_property( CACHE SIXTRACKL_ENABLE_SC_BUNCHED_MAP
+        PROPERTY HELPSTRING "Enable tracking bunched  space-charge maps" )
+
+    set_property( CACHE SIXTRACKL_ENABLE_SC_COASTING_MAP
+        PROPERTY HELPSTRING "Enable tracking coasting space-charge maps" )
+
+    set_property( CACHE SIXTRACKL_ENABLE_TRICUB_MAP
+        PROPERTY HELPSTRING "Enable tracking be_tricub maps" )
+
+    set_property( CACHE SIXTRACKL_ENABLE_BEAM_FIELDS_4D_MAP
+                        SIXTRACKL_ENABLE_BEAM_FIELDS_6D_MAP
+                        SIXTRACKL_ENABLE_SC_COASTING_MAP
+                        SIXTRACKL_ENABLE_SC_BUNCHED_MAP
+                        SIXTRACKL_ENABLE_TRICUB_MAP
+        PROPERTY STRINGS enabled skip disabled runtime_check )
+
+    # ==========================================================================
+    # Global limit check :
+
+    set_property( CACHE SIXTRACKL_GLOBAL_LIMIT_RECT_CHECK
+        PROPERTY HELPSTRING "Enable global LimitRect check" )
+
+    set_property( CACHE SIXTRACKL_GLOBAL_LIMIT_RECT_CHECK
+        PROPERTY STRINGS enabled disabled runtime_check )
+
+
+    set_property( CACHE SIXTRACKL_GLOBAL_LIMIT_RECT_MIN_X
+        PROPERTY HELPSTRING "Global LimitRect check after min_x [m]" )
+
+    set_property( CACHE SIXTRACKL_GLOBAL_LIMIT_RECT_MAX_X
+        PROPERTY HELPSTRING "Global LimitRect check after max_x [m]" )
+
+    set_property( CACHE SIXTRACKL_GLOBAL_LIMIT_RECT_MIN_Y
+        PROPERTY HELPSTRING "Global LimitRect check after min_y [m]" )
+
+    set_property( CACHE SIXTRACKL_GLOBAL_LIMIT_RECT_MAX_Y
+        PROPERTY HELPSTRING "Global LimitRect check after max_y [m]" )
+
+    set_property( CACHE SIXTRACKL_GLOBAL_LIMIT_RECT_MIN_LENGTH
+        PROPERTY HELPSTRING "Global LimitRect check min length  [m]" )
+
+endif()
