fcb608676fdd7deaa8782f7c6a103998dffb9411
Martin Schwinzerl <martin.schwinzerl@lefou.at>
2020-01-21 23:13:23 +0100

    common: starts transition from OBJECT_TYPE_INVALID to OBJECT_TYPE_ILLEGAL

--- sixtracklib/common/internal/objects_type_id.h
+++ sixtracklib/common/internal/objects_type_id.h
@@ -34,7 +34,8 @@ typedef enum NS(object_type_values_e)
     NS(OBJECT_TYPE_ELEM_BY_ELEM_CONF)     =      65534,
     NS(OBJECT_TYPE_NODE_ID)               =      65535,
     NS(OBJECT_TYPE_LAST_AVAILABLE)        =      65535,
-    NS(OBJECT_TYPE_INVALID)               = 0x7fffffff
+    NS(OBJECT_TYPE_INVALID)               = 0x7fffffff,
+    NS(OBJECT_TYPE_ILLEGAL)               = 0x7fffffff
 }
 NS(object_type_values_t);
 
