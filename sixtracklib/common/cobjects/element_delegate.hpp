9a7f5eefa455cd7a1d744e85eea12bdeca6a81fe
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2020-02-26 17:16:02 +0100

    common: adds (obsolete) delegate handler -> has been superseeded by obj_convert, is added only for reference

---
+++ sixtracklib/common/cobjects/element_delegate.hpp
@@ -0,0 +1,38 @@
+#ifndef SIXTRACKLIB_COMMON_COBJECTS_ELEMENT_DELEGATE_CXX_HPP__
+#define SIXTRACKLIB_COMMON_COBJECTS_ELEMENT_DELEGATE_CXX_HPP__
+
+#if !defined( SIXTRL_NO_INCLUDES )
+    #include "sixtracklib/common/definitions.h"
+#endif /* !defined( SIXTRL_NO_INCLUDES ) */
+
+#if defined( __cplusplus )
+
+#if !defined( SIXTRL_NO_SYSTEM_INCLUDES )
+    #include <algorithm>
+    #include <cstdlib>
+    #include <stdint.h>
+    #include <limits>
+    #include <type_traits>
+
+    #if !defined( _GPUCODE )
+        #include <vector>
+    #endif /* !defiend( _GPUCODE ) */
+#endif /* !defined( SIXTRL_NO_SYSTEM_INCLUDES ) */
+
+#if !defined( SIXTRL_NO_INCLUDES )
+    #include "sixtracklib/common/definitions.h"
+    #include "sixtracklib/common/control/definitions.h"
+    #include "sixtracklib/common/internal/obj_store_traits.hpp"
+    #include "sixtracklib/common/internal/obj_type_id_traits.hpp"
+    #include "sixtracklib/common/internal/obj_data_type_traits.hpp"
+#endif /* !defined( SIXTRL_NO_INCLUDES ) */
+
+#if defined( __cplusplus )
+namespace SIXTRL_CXX_NAMESPACE
+{
+
+
+}
+#endif /* defined( __cplusplus ) */
+
+#endif /* SIXTRACKLIB_COMMON_COBJECTS_ELEMENT_DELEGATE_CXX_HPP__ */
