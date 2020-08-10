33d965694cc37c5af330fe97f4038497535473e0
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2020-08-10 18:19:12 +0200

    sixtracklib: refactors global definitions.h file to use sixtracklib_base/definitions.h

---
+++ sixtracklib/definitions.h
@@ -0,0 +1,9 @@
+#ifndef SIXTRACKLIB_DEFINITIONS_H__
+#define SIXTRACKLIB_DEFINITIONS_H__
+
+#if !defined( SIXTRL_NO_INCLUDES )
+    #include "sixtracklib_base/definitions.h"
+    #include "sixtracklib/export.h"
+#endif /* !defined( SIXTRL_NO_INCLUDES ) */
+
+#endif /* SIXTRACKLIB_DEFINITIONS_H__ */
