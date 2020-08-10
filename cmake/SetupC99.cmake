74d2c5aec8797285ee4663c9dba6f763c16a2045
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2020-08-10 17:39:17 +0200

    cmake: moves setup of c99 namespace macos to C99 cmake setup module

--- cmake/SetupC99.cmake
+++ cmake/SetupC99.cmake
@@ -12,4 +12,24 @@ if(  NOT SETUP_C99_FINISHED )
         string( REPLACE " " ";"
                 SIXTRACKLIB_C99_FLAGS ${SIXTRACKL_DEFAULT_C99_FLAGS} )
     endif()
+
+    # --------------------------------------------------------------------------
+    # Setup C99 namespace
+
+    if( NOT SIXTRACKL_C_NAMESPACE_PREFIX )
+        set( SIXTRACKL_C_NAMESPACE_PREFIX "st_" )
+    endif()
+
+    list( APPEND SIXTRACKLIB_NAMESPACE_FLAGS
+          "__NAMESPACE=${SIXTRACKL_C_NAMESPACE_PREFIX}" )
+
+    set( SIXTRACKL_C_NAMESPACE_STR ${SIXTRACKL_C_NAMESPACE_PREFIX} )
+
+    if( SIXTRACKL_ENABLE_PYTHON )
+        set( PY_SIXTRL_C99_NAMESPACE_STR "${SIXTRACKL_C_NAMESPACE_STR}" )
+        mark_as_advanced( PY_SIXTRL_C99_NAMESPACE_STR )
+
+        set( PY_SIXTRL_C99_NAMESPACE_PREFIX_STR "${SIXTRACKL_C_NAMESPACE_PREFIX}" )
+        mark_as_advanced( PY_SIXTRL_C99_NAMESPACE_PREFIX_STR )
+    endif()
 endif()
