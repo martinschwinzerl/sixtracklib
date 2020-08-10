bc054bfbf414f1f4049827c62077bd781836f987
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2020-08-10 17:39:37 +0200

    cmake: moves setup of c++ namespace to C++ cmake setup module

--- cmake/SetupCxx.cmake
+++ cmake/SetupCxx.cmake
@@ -4,22 +4,31 @@ if(  NOT SETUP_CXX_FINISHED )
     message( STATUS "---- Processing cmake/SetupCxx.cmake" )
 
     # --------------------------------------------------------------------------
-    # Add C++11 to the list of supported bindings / languages
-
-    list( APPEND SIXTRACKLIB_SUPPORTED_BINDINGS "CXX" )
-
-    if( SIXTRACKL_ENABLE_CXX )
-        list( APPEND SIXTRACKLIB_SUPPORTED_BINDINGS_VALUES "1" )
-    else()
-        list( APPEND SIXTRACKLIB_SUPPORTED_BINDINGS_VALUES "0" )
-    endif()
-
-    # --------------------------------------------------------------------------
     # Prepare default cxx compiler flags
 
     set( SIXTRACKLIB_CXX_FLAGS )
     if( SIXTRACKL_DEFAULT_CXX_FLAGS )
         string( REPLACE " " ";" SIXTRACKLIB_CXX_FLAGS
                 ${SIXTRACKL_DEFAULT_CXX_FLAGS} )
+    endif()
+
+    # --------------------------------------------------------------------------
+    # Setup C99 namespace
+
+    if( NOT SIXTRACKL_CXX_NAMESPACE )
+        set( SIXTRACKL_CXX_NAMESPACE "sixtrack" )
+    endif()
+
+    set( SIXTRACKL_CXX_NAMESPACE_PREFIX "${SIXTRACKL_CXX_NAMESPACE}" )
+
+    list( APPEND SIXTRACKLIB_NAMESPACE_FLAGS
+          "SIXTRL_NAMESPACE=${SIXTRACKL_CXX_NAMESPACE}" )
+
+    if( SIXTRACKL_ENABLE_PYTHON )
+        set( PY_SIXTRL_CXX_NAMESPACE_STR "${SIXTRACKL_CXX_NAMESPACE}" )
+        mark_as_advanced( PY_SIXTRL_CXX_NAMESPACE_STR )
+
+        set( PY_SIXTRL_CXX_NAMESPACE_PREFIX "${SIXTRACKL_CXX_NAMESPACE_PREFIX}" )
+        mark_as_advanced( PY_SIXTRL_CXX_NAMESPACE_PREFIX )
     endif()
 endif()
