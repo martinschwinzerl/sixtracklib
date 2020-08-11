a6d7a3b1ab593157c20d67240f01f9d8188c9242
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2020-08-11 10:21:17 +0200

    sixtracklib/backends: adds error printing methods to cuda_tools.h

--- sixtracklib/backends/cuda/plugin/cuda_tools.h
+++ sixtracklib/backends/cuda/plugin/cuda_tools.h
@@ -7,6 +7,10 @@
 
 #if !defined( SIXTRL_NO_SYSTEM_INCLUDES )
     #include <cuda.h>
+    #if defined( __cplusplus )
+        #include <ostream>
+        #include <stdexcept>
+    #endif /* defined( __cplusplus ) */
 #endif /* !defined( SIXTRL_NO_SYSTEM_INCLUDES ) */
 
 #if !defined( _GPUCODE ) && defined( __cplusplus )
@@ -18,7 +22,7 @@ extern "C" {
 SIXTRL_STATIC SIXTRL_HOST_FN void NS(Cuda_init_thread)(
     unsigned int const flags );
 
-SIXTRL_STATIC SIXTRL_HOST_FN NS(status_t) NS(Cuda_curesult_to_status)( 
+SIXTRL_STATIC SIXTRL_HOST_FN NS(status_t) NS(Cuda_curesult_to_status)(
     CUresult const result ) SIXTRL_NOEXCEPT;
 
 #endif /* _GPUCODE */
@@ -26,6 +30,21 @@ SIXTRL_STATIC SIXTRL_HOST_FN NS(status_t) NS(Cuda_curesult_to_status)(
 #if !defined( _GPUCODE ) && defined( __cplusplus )
 }
 #endif /* C++, Host */
+
+#if !defined( _GPUCODE ) && defined( __cplusplus )
+namespace SIXTRL_CXX_NAMESPACE
+{
+    SIXTRL_STATIC SIXTRL_HOST_FN std::ostream&
+    CudaError_print_error_name(
+        std::ostream& SIXTRL_RESTRICT_REF ostr, ::CUresult const err_code,
+        std::string const& SIXTRL_RESTRICT_REF default_str = std::string{} );
+
+    SIXTRL_STATIC SIXTRL_HOST_FN std::ostream&
+    CudaError_print_error_description(
+        std::ostream& SIXTRL_RESTRICT_REF ostr, ::CUresult const err_code,
+        std::string const& SIXTRL_RESTRICT_REF default_str = std::string{} );
+}
+#endif /* C++ */
 
 /* ------------------------------------------------------------------------ */
 
@@ -42,17 +61,70 @@ SIXTRL_INLINE void NS(Cuda_init_thread)( unsigned int const flags )
     ( void )ret;
 }
 
-SIXTRL_INLINE NS(status_t) NS(Cuda_curesult_to_status)( 
+SIXTRL_INLINE NS(status_t) NS(Cuda_curesult_to_status)(
     CUresult const result ) SIXTRL_NOEXCEPT
 {
-    return ( result == CUDA_SUCCESS ) 
+    return ( result == CUDA_SUCCESS )
         ? ( NS(status_t) )SIXTRL_STATUS_SUCCESS
         : ( NS(status_t) )SIXTRL_STATUS_GENERAL_FAILURE;
 }
 
 #endif /* _GPUCODE */
-
 #if !defined( _GPUCODE ) && defined( __cplusplus )
 }
 #endif /* C++, Host */
+
+#if !defined( _GPUCODE ) && defined( __cplusplus )
+namespace SIXTRL_CXX_NAMESPACE
+{
+    SIXTRL_INLINE std::ostream& CudaError_print_error_name(
+        std::ostream& SIXTRL_RESTRICT_REF ostr, ::CUresult const err_code,
+        std::string const& SIXTRL_RESTRICT_REF default_str )
+    {
+        if( err_code != ::CUDA_SUCCESS )
+        {
+            char* error_name = nullptr;
+            ::CUresult const ret = ::cuGetErrorName( err_code, &error_name );
+            SIXTRL_ASSERT( ret == ::CUDA_SUCCESS );
+            ( void )ret;
+
+            if( error_name != nullptr )
+            {
+                ostr << error_name;
+            }
+            else
+            {
+                ostr << default_str;
+            }
+        }
+
+        return ostr;
+    }
+
+    SIXTRL_INLINE std::ostream& CudaError_print_error_description(
+        std::ostream& SIXTRL_RESTRICT_REF ostr, ::CUresult const err_code,
+        std::string const& SIXTRL_RESTRICT_REF default_str )
+    {
+        if( err_code != ::CUDA_SUCCESS )
+        {
+            char* error_msg = nullptr;
+            ::CUresult const ret = ::cuGetErrorString( err_code, &error_msg );
+            SIXTRL_ASSERT( ret == ::CUDA_SUCCESS );
+            ( void )ret;
+
+            if( error_msg != nullptr )
+            {
+                ostr << error_msg;
+            }
+            else
+            {
+                ostr << default_str;
+            }
+        }
+
+        return ostr;
+    }
+}
+#endif /* C++ */
+
 #endif /* SIXTRACKLIB_BACKENDS_CUDA_PLUGIN_CUDA_TOOLS_H__ */
