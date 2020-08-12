1e9deb87d683fed7236c8417eb8ee1137c9a5830
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2020-08-12 19:07:55 +0200

    sixtracklib/cuda: adds convenience macro to run driver API methods

--- sixtracklib/backends/cuda/plugin/cuda_tools.h
+++ sixtracklib/backends/cuda/plugin/cuda_tools.h
@@ -3,12 +3,14 @@
 
 #if !defined( SIXTRL_NO_INCLUDES )
     #include "sixtracklib/backends/cuda/definitions.h"
+    #include "sixtracklib/backends/cuda/dlib.h"
 #endif /* C++ */
 
 #if !defined( SIXTRL_NO_SYSTEM_INCLUDES )
     #include <cuda.h>
     #if defined( __cplusplus )
         #include <ostream>
+        #include <sstream>
         #include <stdexcept>
     #endif /* defined( __cplusplus ) */
 #endif /* !defined( SIXTRL_NO_SYSTEM_INCLUDES ) */
@@ -45,6 +47,35 @@ namespace SIXTRL_CXX_NAMESPACE
         std::string const& SIXTRL_RESTRICT_REF default_str = std::string{} );
 }
 #endif /* C++ */
+
+#if !defined( SIXTRL_CUDA_DRIVER_API_NOTHROW_CALL )
+    #define SIXTRL_CUDA_DRIVER_API_NOTHROW_CALL( fn, args ) \
+        do { ::CUresult const ret = fn args ; \
+             SIXTRL_ASSERT( ret == ::CUDA_SUCCESS ); }while( 0 )
+#endif /* !defined( SIXTRL_CUDA_DRIVER_API_NOTHROW_CALL ) */
+
+#if !defined( SIXTRL_CUDA_DRIVER_API_CALL )
+    #if defined( SIXTRL_CUDA_PLUGIN_EXCEPTION_ON_ERROR ) && \
+        SIXTRL_CUDA_PLUGIN_EXCEPTION_ON_ERROR == 1
+
+        #define SIXTRL_CUDA_DRIVER_API_CALL( fn, args ) \
+        do {\
+            ::CUresult const ret = fn args ; \
+            if( ret != ::CUDA_SUCCESS ) { \
+                std::ostringstream a2str; \
+                a2str << "error during call to fn" << #fn << ": "; \
+                SIXTRL_CXX_NAMESPACE::CudaError_print_error_name( a2str, ret, \
+                    "unknown_error_name" ) << ": "; \
+                SIXTRL_CXX_NAMESPACE::CudaError_print_error_description( \
+                    a2str, ret ); \
+                throw std::runtime_error( a2str.str() ); } }while( 0 )
+    #else /* do not use exceptions! */
+        #define SIXTRL_CUDA_DRIVER_API_CALL( fn, args ) \
+        do { ::CUresult const ret = fn args ; \
+             SIXTRL_ASSERT( ret == ::CUDA_SUCCESS ); }while( 0 )
+    #endif /* use exceptions ? */
+#endif /* !defined( SIXTRL_CUDA_DRIVER_API_CALL ) */
+
 
 /* ------------------------------------------------------------------------ */
 
