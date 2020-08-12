174bb6ebab08419164a93f4c18216eb6007ae78d
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2020-08-12 14:25:17 +0200

    sixtracklib_base/internal: adds header with a designated "illegal" type for storage
    NOTE: using an empty struct as an illegal type instead of, for example, void
          allows to write some expression template metaprogramming rules and especially
          SFINAE based specialization specifically for corner cases where an "illegal"
          type may be encountered -> think types that can not be stored in a CObjects
          buffer and which have no delegate
    
          (void is not accepted in all places where we may need it, i.e. getting a
          sizeof from a void is not defined, etc.)

---
+++ sixtracklib_base/internal/obj_illegal_type.h
@@ -0,0 +1,42 @@
+#ifndef SIXTRACKLIB_BASE_INTERNAL_OBJ_ILLEGAL_TYPE_H__
+#define SIXTRACKLIB_BASE_INTERNAL_OBJ_ILLEGAL_TYPE_H__
+
+#if !defined( SIXTRL_NO_INCLUDES )
+    #include "sixtracklib_base/definitions.h"
+#endif /* !defined( SIXTRL_NO_INCLUDES ) */
+
+#if !defined( SIXTRL_NO_SYSTEM_INCLUDES )
+    #if defined( __cplusplus )
+        #include <type_traits>
+    #endif /* C++ */
+#endif /* !defined( SIXTRL_NO_SYSTEM_INCLUDES ) */
+
+#if defined( __cplusplus )
+namespace SIXTRL_CXX_NAMESPACE
+{
+    struct IllegalType{};
+
+    template< class E > static SIXTRL_FN constexpr bool
+    ObjData_is_specific_illegal_type() SIXTRL_NOEXCEPT
+    {
+        return std::is_same< typename std::decay< E >::type,
+                             IllegalType >::value;
+    }
+}
+
+#if !defined( _GPUCODE )
+extern "C" {
+#endif /* !defined( _GPUCODE ) */
+
+typedef SIXTRL_CXX_NAMESPACE::IllegalType NS(IllegalType);
+
+#if !defined( _GPUCODE )
+}
+#endif /* !defined( _GPUCODE ) */
+
+#else /* !defined( __cplusplus ) */
+
+struct NS(IllegalType);
+
+#endif /* defined( __cplusplus ) */
+#endif /* SIXTRACKLIB_BASE_INTERNAL_OBJ_ILLEGAL_TYPE_H__ */
