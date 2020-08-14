0ac2eb04ab9585f3f2447f64cb0f8611cb5701ea
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2020-08-14 02:46:02 +0200

    sixtracklib_base/control: adds definitions header to control submodule

---
+++ sixtracklib_base/control/definitions.h
@@ -0,0 +1,8 @@
+#ifndef SIXTRACKLIB_BASE_CONTROL_DEFINITIONS_H__
+#define SIXTRACKLIB_BASE_CONTROL_DEFINITIONS_H__
+
+#if !defined( SIXTRL_NO_INCLUDES )
+    #include "sixtracklib_base/definitions.h"
+#endif /* !defined( SIXTRL_NO_INCLUDES ) */
+
+#endif /* SIXTRACKLIB_BASE_CONTROL_DEFINITIONS_H__ */
