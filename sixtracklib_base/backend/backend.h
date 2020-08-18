c0bd5cca5884db0f08ea0f89504c8c26dd1a4fd3
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2020-08-18 14:09:57 +0200

    sixtracklib_base/backend: updates exported C API section
    - provides a typedef for the thread-id; note that at the moment, this
      only works with POSIX threads -> if different implementations are needed,
      different code-paths have to be carved out here

--- sixtracklib_base/backend/backend.h
+++ sixtracklib_base/backend/backend.h
@@ -21,6 +21,8 @@
         #include <string>
         #include <thread>
         #include <unordered_map>
+    #else /* C */
+        #include <pthread.h>
     #endif /* C++ */
 
     #if defined( __unix__ )
@@ -393,16 +395,31 @@ namespace SIXTRL_CXX_NAMESPACE
         bool m_is_available = false;
         bool m_is_ready = false;
     };
-}
+} /* ns: SIXTRL_CXX_NAMESPACE */
+#endif /* C++, Host */
 
-extern "C"
-{
-    typedef SIXTRL_CXX_NAMESPACE::BaseBackend NS(BaseBackend);
-}
+/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+/* !!!!                   Exported Plugin C-API :: Types                !!!! */
+/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
 
-#else /* C++, Host */
+#if defined( __cplusplus ) && !defined( _GPUCODE )
+extern "C" {
+#endif /* C++, Host */
 
+#if defined( __cplusplus ) && !defined( _GPUCODE )
+
+typedef SIXTRL_CXX_NAMESPACE::BaseBackend NS(BaseBackend);
+typedef SIXTRL_CXX_NAMESPACE::BaseBackend::thread_id_type
+        NS(backend_thread_id_t);
+
+#elif !defined( _GPUCODE ) /* C, Host */
+
+/* TODO: Enure portability of C NS(backend_thread_id_t) typedef! */
+struct NS(backend_thread_id_t);
 struct NS(BaseBackend);
 
+#endif /* C++ / C & Host */
+#if defined( __cplusplus ) && !defined( _GPUCODE )
+}
 #endif /* C++, Host */
 #endif /* SIXTRACKLIB_BASE_BACKEND_BACKEND_H__ */
