fb5500301dcefd187aeb25f3c9c0023cdf53ebf9
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2020-08-21 16:08:32 +0200

    sixtracklib_base/internal: adds class ids for the runtime compiled program classes

--- sixtracklib_base/internal/class_id.h
+++ sixtracklib_base/internal/class_id.h
@@ -48,11 +48,14 @@ namespace SIXTRL_CXX_NAMESPACE
 #endif /* C++ */
 
 /* ------------------------------------------------------------------------- */
-/* node: */
+/* sixtracklib_base/backend: */
 
 #if !defined( SIXTRL_CLASS_ID_BACKEND )
     #define SIXTRL_CLASS_ID_BACKEND 0
 #endif /* !defined( SIXTRL_CLASS_ID_BACKEND ) */
+
+/* ------------------------------------------------------------------------- */
+/* sixtracklib_base/node: */
 
 #if !defined( SIXTRL_CLASS_ID_NODE_ID )
     #define SIXTRL_CLASS_ID_NODE_ID 1
@@ -62,6 +65,9 @@ namespace SIXTRL_CXX_NAMESPACE
     #define SIXTRL_CLASS_ID_NODE_INFO 2
 #endif /* !defined( SIXTRL_CLASS_ID_NODE_INFO ) */
 
+/* ------------------------------------------------------------------------- */
+/* sixtracklib_base/control: */
+
 #if !defined( SIXTRL_CLASS_ID_CONTEXT )
     #define SIXTRL_CLASS_ID_CONTEXT 3
 #endif /* !defined( SIXTRL_CLASS_ID_CONTEXT ) */
@@ -70,21 +76,46 @@ namespace SIXTRL_CXX_NAMESPACE
     #define SIXTRL_CLASS_ID_CONTEXT_STORE 4
 #endif /* !defined( SIXTRL_CLASS_ID_CONTEXT_STORE ) */
 
-#if !defined( SIXTRL_CLASS_ID_DEVICE )
-    #define SIXTRL_CLASS_ID_DEVICE 5
-#endif /* !defined( SIXTRL_CLASS_ID_DEVICE ) */
+/* ------------------------------------------------------------------------- */
+/* sixtracklib_base/program: */
+
+#if !defined( SIXTRL_CLASS_ID_PROGRAM_COMPILE_OPTION_ITEM )
+    #define SIXTRL_CLASS_ID_PROGRAM_COMPILE_OPTION_ITEM 5
+#endif /* !defined( SIXTRL_CLASS_ID_PROGRAM_COMPILE_OPTION_ITEM ) */
+
+#if !defined( SIXTRL_CLASS_ID_PROGRAM_COMPILE_OPTIONS )
+    #define SIXTRL_CLASS_ID_PROGRAM_COMPILE_OPTIONS 6
+#endif /* !defined( SIXTRL_CLASS_ID_PROGRAM_COMPILE_OPTIONS ) */
 
 #if !defined( SIXTRL_CLASS_ID_PROGRAM_CODE )
-    #define SIXTRL_CLASS_ID_PROGRAM_CODE 6
+    #define SIXTRL_CLASS_ID_PROGRAM_CODE 7
 #endif /* !defined( SIXTRL_CLASS_ID_PROGRAM_CODE ) */
+
+#if !defined( SIXTRL_CLASS_ID_PROGRAM_INTERMEDIATE_REPR )
+    #define SIXTRL_CLASS_ID_PROGRAM_INTERMEDIATE_REPR 8
+#endif /* !defined( SIXTRL_CLASS_ID_PROGRAM_INTERMEDIATE_REPR ) */
+
+#if !defined( SIXTRL_CLASS_ID_PROGRAM_COMPILED )
+    #define SIXTRL_CLASS_ID_PROGRAM_COMPILED 9
+#endif /* !defined( SIXTRL_CLASS_ID_PROGRAM_COMPILED ) */
+
+#if !defined( SIXTRL_CLASS_ID_PROGRAM_KERNEL )
+    #define SIXTRL_CLASS_ID_PROGRAM_KERNEL 10
+#endif /* !defined( SIXTRL_CLASS_ID_PROGRAM_KERNEL ) */
 
 #if !defined( _GPUCODE )
 #if defined( __cplusplus )
 extern "C" {
 #endif /* c++ */
 
+/* ------------------------------------------------------------------------- */
+/* sixtracklib_base/backend: */
+
 SIXTRL_STATIC_VAR const NS(obj_class_id_t) NS(CLASS_ID_BACKEND) =
     ( NS(obj_class_id_t) )SIXTRL_CLASS_ID_BACKEND;
+
+/* ------------------------------------------------------------------------- */
+/* sixtracklib_base/node: */
 
 SIXTRL_STATIC_VAR const NS(obj_class_id_t) NS(CLASS_ID_NODE_ID) =
     ( NS(obj_class_id_t) )SIXTRL_CLASS_ID_NODE_ID;
@@ -92,17 +123,38 @@ SIXTRL_STATIC_VAR const NS(obj_class_id_t) NS(CLASS_ID_NODE_ID) =
 SIXTRL_STATIC_VAR const NS(obj_class_id_t) NS(CLASS_ID_NODE_INFO) =
     ( NS(obj_class_id_t) )SIXTRL_CLASS_ID_NODE_INFO;
 
+/* ------------------------------------------------------------------------- */
+/* sixtracklib_base/control: */
+
 SIXTRL_STATIC_VAR const NS(obj_class_id_t) NS(CLASS_ID_CONTEXT) =
     ( NS(obj_class_id_t) )SIXTRL_CLASS_ID_CONTEXT;
 
 SIXTRL_STATIC_VAR const NS(obj_class_id_t) NS(CLASS_ID_CONTEXT_STORE) =
     ( NS(obj_class_id_t) )SIXTRL_CLASS_ID_CONTEXT_STORE;
 
-SIXTRL_STATIC_VAR const NS(obj_class_id_t) NS(CLASS_ID_DEVICE) =
-    ( NS(obj_class_id_t) )SIXTRL_CLASS_ID_DEVICE;
+/* ------------------------------------------------------------------------- */
+/* sixtracklib_base/program: */
+
+SIXTRL_STATIC_VAR const NS(obj_class_id_t)
+    NS(CLASS_ID_PROGRAM_COMPILE_OPTION_ITEM) = ( NS(obj_class_id_t)
+        )SIXTRL_CLASS_ID_PROGRAM_COMPILE_OPTION_ITEM;
+
+SIXTRL_STATIC_VAR const NS(obj_class_id_t)
+    NS(CLASS_ID_PROGRAM_COMPILE_OPTIONS) = ( NS(obj_class_id_t)
+        )SIXTRL_CLASS_ID_PROGRAM_COMPILE_OPTIONS;
 
 SIXTRL_STATIC_VAR const NS(obj_class_id_t) NS(CLASS_ID_PROGRAM_CODE) =
     ( NS(obj_class_id_t) )SIXTRL_CLASS_ID_PROGRAM_CODE;
+
+SIXTRL_STATIC_VAR const NS(obj_class_id_t)
+    NS(CLASS_ID_PROGRAM_INTERMEDIATE_REPR) = ( NS(obj_class_id_t)
+        )SIXTRL_CLASS_ID_PROGRAM_INTERMEDIATE_REPR;
+
+SIXTRL_STATIC_VAR const NS(obj_class_id_t) NS(CLASS_ID_PROGRAM_COMPILED) =
+    ( NS(obj_class_id_t) )SIXTRL_CLASS_ID_PROGRAM_COMPILED;
+
+SIXTRL_STATIC_VAR const NS(obj_class_id_t) NS(CLASS_ID_PROGRAM_KERNEL) =
+    ( NS(obj_class_id_t) )SIXTRL_CLASS_ID_PROGRAM_KERNEL;
 
 #if defined( __cplusplus )
 }
@@ -112,8 +164,14 @@ SIXTRL_STATIC_VAR const NS(obj_class_id_t) NS(CLASS_ID_PROGRAM_CODE) =
 #if defined( __cplusplus )
 namespace SIXTRL_CXX_NAMESPACE
 {
+    /* --------------------------------------------------------------------- */
+    /* sixtracklib_base/backend: */
+
     static constexpr obj_class_id_t CLASS_ID_BACKEND =
         static_cast< obj_class_id_t >( SIXTRL_CLASS_ID_BACKEND );
+
+    /* --------------------------------------------------------------------- */
+    /* sixtracklib_base/node: */
 
     static constexpr obj_class_id_t CLASS_ID_NODE_ID =
         static_cast< obj_class_id_t >( SIXTRL_CLASS_ID_NODE_ID );
@@ -121,17 +179,38 @@ namespace SIXTRL_CXX_NAMESPACE
     static constexpr obj_class_id_t CLASS_ID_NODE_INFO =
         static_cast< obj_class_id_t >( SIXTRL_CLASS_ID_NODE_INFO );
 
+    /* --------------------------------------------------------------------- */
+    /* sixtracklib_base/control: */
+
     static constexpr obj_class_id_t CLASS_ID_CONTEXT =
         static_cast< obj_class_id_t >( SIXTRL_CLASS_ID_CONTEXT );
 
     static constexpr obj_class_id_t CLASS_ID_CONTEXT_STORE =
         static_cast< obj_class_id_t >( SIXTRL_CLASS_ID_CONTEXT_STORE );
 
-    static constexpr obj_class_id_t CLASS_ID_DEVICE =
-        static_cast< obj_class_id_t >( SIXTRL_CLASS_ID_DEVICE );
+    /* --------------------------------------------------------------------- */
+    /* sixtracklib_base/program: */
 
-    static constexpr obj_class_id_t CLASS_ID_PROGRAM_CODE =
+    static constexpr obj_class_id_t CLASS_ID_PROGRAM_COMPILE_OPTION_ITEM =
+        static_cast< obj_class_id_t >(
+            SIXTRL_CLASS_ID_PROGRAM_COMPILE_OPTION_ITEM );
+
+    static constexpr obj_class_id_t CLASS_ID_PROGRAM_COMPILE_OPTIONS =
+        static_cast< obj_class_id_t >(
+            SIXTRL_CLASS_ID_PROGRAM_COMPILE_OPTIONS );
+
+    static constexpr obj_class_id_t CLASS_ID_PROGRAM_CODE  =
         static_cast< obj_class_id_t >( SIXTRL_CLASS_ID_PROGRAM_CODE );
+
+    static constexpr obj_class_id_t CLASS_ID_PROGRAM_INTERMEDIATE_REPR =
+        static_cast< obj_class_id_t >(
+            SIXTRL_CLASS_ID_PROGRAM_INTERMEDIATE_REPR );
+
+    static constexpr obj_class_id_t CLASS_ID_PROGRAM_COMPILED =
+        static_cast< obj_class_id_t >( SIXTRL_CLASS_ID_PROGRAM_COMPILED );
+
+    static constexpr obj_class_id_t CLASS_ID_PROGRAM_KERNEL  =
+        static_cast< obj_class_id_t >( SIXTRL_CLASS_ID_PROGRAM_KERNEL );
 
 } /* ns: SIXTRL_CXX_NAMESPACE */
 #endif /* C++ */
