4fea1258044ec6d99b8d59f4082acb10469273c3
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2020-08-21 16:06:46 +0200

    sixtracklib/cuda: precondition build paths on existance and enabling of cuda

--- sixtracklib/backends/cuda/cuda.h
+++ sixtracklib/backends/cuda/cuda.h
@@ -5,10 +5,6 @@
     #include "sixtracklib/backends/cuda/definitions.h"
     #include "sixtracklib/backends/cuda/dlib.h"
     #include "sixtracklib/backends/cuda/backend.h"
-
-    #if defined( SIXTRACKLIB_CUDA_PLUGIN_BUILT ) && \
-                 SIXTRACKLIB_CUDA_PLUGIN_BUILT == 1
-        #include "sixtracklib/backends/cuda/plugin.h"
-    #endif /* plugin */
+    #include "sixtracklib/backends/cuda/plugin.h"
 #endif /* !defined( SIXTRL_NO_INCLUDES ) */
 #endif /* SIXTRACKLIB_BACKENDS_CUDA_CUDA_H__ */
--- sixtracklib/backends/cuda/plugin.h
+++ sixtracklib/backends/cuda/plugin.h
@@ -5,13 +5,12 @@
     #include "sixtracklib/backends/cuda/definitions.h"
     #include "sixtracklib/backends/cuda/dlib.h"
 
-    #if defined( SIXTRACKL_ENABLE_BACKEND_CUDA ) && \
-        SIXTRACKL_ENABLE_BACKEND_CUDA == 1
-
-        #include "sixtracklib/backends/cuda/node.h"
-        #include "sixtracklib/backends/cuda/controller.h"
-        #include "sixtracklib/backends/cuda/track_job.h"
+    #if defined( SIXTRL_CUDA_PLUGIN_BUILT ) && ( SIXTRL_CUDA_PLUGIN_BUILT == 1 )
+        #include "sixtracklib/backends/cuda/plugin/context.h"
+        #include "sixtracklib/backends/cuda/plugin/node.h"
+//         #include "sixtracklib/backends/cuda/plugin/controller.h"
+//         #include "sixtracklib/backends/cuda/plugin/track_job.h"
 
     #endif /* CUDA enabled */
-#endif /* !defined( SIXTRL_NO_INCLUDES ) */
+#endif /* !defined( SIXTRL_CUDA_PLUGIN_BUILT ) */
 #endif /* SIXTRACKLIB_BACKENDS_CUDA_PLUGIN_H__ */
