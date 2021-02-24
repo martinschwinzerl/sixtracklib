ba3cef9048aa5207dab4c8f6536ea8aafaa1007d
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2021-02-24 12:13:06 +0100

    sixtracklib: updates convenience headers

--- sixtracklib/sixtracklib.h
+++ sixtracklib/sixtracklib.h
@@ -1,18 +1,70 @@
 #ifndef SIXTRACKLIB_SIXTRACKLIB_H__
 #define SIXTRACKLIB_SIXTRACKLIB_H__
 
-#if !defined( __NAMESPACE )
-    #define __NAMESPACE st_
-    #define __SIXTRACKLIB_SIXTRACKLIB_UNDEF_NAMESPACE 1
-#endif /* !defined( __NAMESPACE ) */
-
-/* ------------------------------------------------------------------------- */
-
-#include "sixtracklib/common/generated/namespace.h"
-#include "sixtracklib/common/generated/config.h"
 #include "sixtracklib/common/definitions.h"
+#include "sixtracklib/common/generated/namespace.h"
+
+#include "sixtracklib/common/backends/definitions.h"
+#include "sixtracklib/common/backends/definitions_cuda.h"
+#include "sixtracklib/common/backends/definitions_opencl.h"
+#include "sixtracklib/common/backends/helper_methods.h"
+#include "sixtracklib/common/backends/backends.h"
+
+#include "sixtracklib/common/beam_elements/definitions.h"
+#include "sixtracklib/common/beam_elements/cobj_type_ids.h"
+#include "sixtracklib/common/beam_elements/all_beam_elements.h"
+#include "sixtracklib/common/beam_elements/all_tracking_maps.h"
+
+#if defined( SIXTRL_DEMOTRACK_ENABLED ) && ( SIXTRL_DEMOTRACK_ENABLED == 1 )
+#include "sixtracklib/common/beam_elements/demotrack.h"
+#endif /* defined( SIXTRL_DEMOTRACK_ENABLED )  */
+
+#include "sixtracklib/common/cobjects/definitions.h"
+#include "sixtracklib/common/cobjects/address_manipulations.h"
+#include "sixtracklib/common/cobjects/cbuffer.h"
+#include "sixtracklib/common/cobjects/cbuffer_view.hpp"
+#include "sixtracklib/common/cobjects/cbuffer.hpp"
+#include "sixtracklib/common/cobjects/cobj_illegal_type.h"
+#include "sixtracklib/common/cobjects/cobj_type_traits.h"
+#include "sixtracklib/common/cobjects/flat_buffer.h"
+#include "sixtracklib/common/cobjects/garbage_range.h"
+#include "sixtracklib/common/cobjects/index_object.h"
+
+#include "sixtracklib/common/control/definitions.h"
+#include "sixtracklib/common/control/node_id.h"
+#include "sixtracklib/common/control/node_info.h"
+
+#include "sixtracklib/common/generated/config.h"
 #include "sixtracklib/common/generated/path.h"
 #include "sixtracklib/common/generated/modules.h"
+
+#include "sixtracklib/common/internal/file_tools.h"
+#include "sixtracklib/common/internal/math_constants.h"
+#include "sixtracklib/common/internal/math_factorial.h"
+#include "sixtracklib/common/internal/math_functions.h"
+#include "sixtracklib/common/internal/math_interpol.h"
+#include "sixtracklib/common/internal/math_qgauss.h"
+#include "sixtracklib/common/internal/physics_constants.h"
+#include "sixtracklib/common/internal/type_arithmetic_helpers.h"
+#include "sixtracklib/common/internal/type_comparison_helpers.h"
+#include "sixtracklib/common/internal/tools.h"
+
+#include "sixtracklib/common/particles/definitions.h"
+#include "sixtracklib/common/particles/cobj_type_ids.h"
+#include "sixtracklib/common/particles/particle.h"
+#include "sixtracklib/common/particles/particles.h"
+#include "sixtracklib/common/particles/particles_addr.h"
+#include "sixtracklib/common/particles/particle_helper.h"
+
+#if defined( SIXTRL_DEMOTRACK_ENABLED ) && ( SIXTRL_DEMOTRACK_ENABLED == 1 )
+#include "sixtracklib/common/particles/demotrack.h"
+#endif /* defined( SIXTRL_DEMOTRACK_ENABLED )  */
+
+#include "sixtracklib/common/track/definitions.h"
+#include "sixtracklib/common/track/track_config.h"
+#include "sixtracklib/common/track/track.h"
+
+/*
 #include "sixtracklib/common/buffer/mem_pool.h"
 #include "sixtracklib/common/buffer/managed_buffer_minimal.h"
 #include "sixtracklib/common/buffer/managed_buffer_remap.h"
@@ -68,35 +120,30 @@
 #include "sixtracklib/common/particles.h"
 #include "sixtracklib/common/track_job.h"
 #include "sixtracklib/common/track_job_cpu.h"
+*/
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 #if defined( SIXTRACKLIB_ENABLE_MODULE_OPENCL ) && \
            ( SIXTRACKLIB_ENABLE_MODULE_OPENCL == 1 )
-
+/*
     #include "sixtracklib/opencl/opencl.h"
     #include "sixtracklib/opencl/argument.h"
     #include "sixtracklib/opencl/context.h"
     #include "sixtracklib/opencl/track_job_cl.h"
-
+*/
 #endif /* defined( SIXTRACKLIB_ENABLE_MODULE_OPENCL ) */
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 #if defined( SIXTRACKLIB_ENABLE_MODULE_CUDA ) && \
            ( SIXTRACKLIB_ENABLE_MODULE_CUDA == 1 )
-
+/*
     #include "sixtracklib/cuda/definitions.h"
     #include "sixtracklib/cuda/control/kernel_config.h"
     #include "sixtracklib/cuda/argument.h"
     #include "sixtracklib/cuda/controller.h"
     #include "sixtracklib/cuda/track_particles_kernel_c_wrapper.h"
-
+*/
 #endif /* defined( SIXTRACKLIB_ENABLE_MODULE_OPENCL ) */
-
-
-/* ------------------------------------------------------------------------- */
-
 #endif /* SIXTRACKLIB_SIXTRACKLIB_H__ */
-
-/* end: sixtracklib/sixtracklib.h */
--- sixtracklib/sixtracklib.hpp
+++ sixtracklib/sixtracklib.hpp
@@ -7,46 +7,30 @@
 #include "sixtracklib/common/generated/config.h"
 #include "sixtracklib/common/definitions.h"
 
-#include "sixtracklib/common/buffer.hpp"
-#include "sixtracklib/common/be_drift/be_drift.hpp"
-#include "sixtracklib/common/be_monitor/be_monitor.hpp"
-#include "sixtracklib/common/be_limit/be_limit_rect.hpp"
-#include "sixtracklib/common/be_limit/be_limit_ellipse.hpp"
-#include "sixtracklib/common/be_dipedge/be_dipedge.hpp"
-#include "sixtracklib/common/output/output_buffer.hpp"
-#include "sixtracklib/common/particles.hpp"
-
 #include "sixtracklib/sixtracklib.h"
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-#if defined( SIXTRACKLIB_ENABLE_MODULE_SIMD ) && \
-           ( SIXTRACKLIB_ENABLE_MODULE_SIMD == 1 )
-
-#endif /* defined( SIXTRACKLIB_ENABLE_MODULE_SIMD ) */
-
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-
-#if defined( SIXTRACKLIB_ENABLE_MODULE_OPENCL ) && \
-           ( SIXTRACKLIB_ENABLE_MODULE_OPENCL == 1 )
-
-    #include "sixtracklib/opencl/opencl.hpp"
-
-#endif /* defined( SIXTRACKLIB_ENABLE_MODULE_OPENCL ) */
+// #if defined( SIXTRACKLIB_ENABLE_MODULE_OPENCL ) && \
+//            ( SIXTRACKLIB_ENABLE_MODULE_OPENCL == 1 )
+//
+//     #include "sixtracklib/opencl/opencl.hpp"
+//
+// #endif /* defined( SIXTRACKLIB_ENABLE_MODULE_OPENCL ) */
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-#if defined( SIXTRACKLIB_ENABLE_MODULE_CUDA ) && \
-           ( SIXTRACKLIB_ENABLE_MODULE_CUDA == 1 )
+// #if defined( SIXTRACKLIB_ENABLE_MODULE_CUDA ) && \
+//            ( SIXTRACKLIB_ENABLE_MODULE_CUDA == 1 )
 
-#include "sixtracklib/cuda/control/kernel_config.hpp"
-#include "sixtracklib/cuda/control/node_info.hpp"
-
-#include "sixtracklib/cuda/argument.hpp"
-#include "sixtracklib/cuda/controller.hpp"
-#include "sixtracklib/cuda/track_job.hpp"
-
-#endif /* defined( SIXTRACKLIB_ENABLE_MODULE_OPENCL ) */
+// #include "sixtracklib/cuda/control/kernel_config.hpp"
+// #include "sixtracklib/cuda/control/node_info.hpp"
+//
+// #include "sixtracklib/cuda/argument.hpp"
+// #include "sixtracklib/cuda/controller.hpp"
+// #include "sixtracklib/cuda/track_job.hpp"
+//
+// #endif /* defined( SIXTRACKLIB_ENABLE_MODULE_OPENCL ) */
 
 
 /* ------------------------------------------------------------------------- */
