eb4465178a039dda8af6057c7bf24b026ef13a3b
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2021-02-24 11:49:30 +0100

    common: adds convenience header for summarizing all cobjects ids

---
+++ sixtracklib/common/cobj_type_ids.h
@@ -0,0 +1,10 @@
+#ifndef SIXTRACKLIB_COMMON_COBJ_TYPE_IDS_H__
+#define SIXTRACKLIB_COMMON_COBJ_TYPE_IDS_H__
+
+#if !defined( SIXTRL_NO_INCLUDES )
+    #include "sixtracklib/common/definitions.h"
+#endif /* SIXTRACKLIB_COMMON_COBJ_TYPE_IDS_H__ */
+
+#if !defined( SIXTRL_COBJECTS_TYPE_
+
+#endif /* SIXTRACKLIB_COMMON_COBJ_TYPE_IDS_H__ */
