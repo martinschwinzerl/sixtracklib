1e2d9e67e4faf8fa300c94288a9b4166de732e66
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2021-02-24 12:12:50 +0100

    testlib: adds convenience headers for testlib

---
+++ sixtracklib/testlib.h
@@ -0,0 +1,29 @@
+#ifndef SIXTRACKLIB_TESTLIB_TESTLIB_H__
+#define SIXTRACKLIB_TESTLIB_TESTLIB_H__
+
+#if !defined( SIXTRL_NO_INCLUDES )
+
+    #include "sixtracklib/testlib/common/beam_elements/beamfields.h"
+    #include "sixtracklib/testlib/common/beam_elements/cavity.h"
+    #include "sixtracklib/testlib/common/beam_elements/dipedge.h"
+    #include "sixtracklib/testlib/common/beam_elements/drift.h"
+    #include "sixtracklib/testlib/common/beam_elements/end_tracking.h"
+    #include "sixtracklib/testlib/common/beam_elements/limit.h"
+    #include "sixtracklib/testlib/common/beam_elements/monitor.h"
+    #include "sixtracklib/testlib/common/beam_elements/multipole.h"
+//     #include "sixtracklib/testlib/common/beam_elements/rf_multipole.h"
+    #include "sixtracklib/testlib/common/beam_elements/srotation.h"
+    #include "sixtracklib/testlib/common/beam_elements/xy_shift.h"
+
+    #include "sixtracklib/testlib/common/cobjects/cobj_no_ptrs.h"
+    #include "sixtracklib/testlib/common/cobjects/cobj_dataptrs.h"
+
+    #include "sixtracklib/testlib/common/particles/particle.h"
+    #include "sixtracklib/testlib/common/particles/particles.h"
+
+    #include "sixtracklib/testlib/common/track/sixtrack_sequ_by_sequ.h"
+    #include "sixtracklib/testlib/common/track/cmp_elem_by_elem.h"
+
+#endif /* !defined( SIXTRL_NO_INCLUDES ) */
+
+#endif /* SIXTRACKLIB_TESTLIB_TESTLIB_H__ */
---
+++ sixtracklib/testlib.hpp
@@ -0,0 +1,13 @@
+#ifndef SIXTRACKLIB_TESTLIB_TESTLIB_CXX_HPP__
+#define SIXTRACKLIB_TESTLIB_TESTLIB_CXX_HPP__
+
+#if !defined( SIXTRL_NO_INCLUDES )
+    #include "sixtracklib/testlib.h"
+#endif /* !defined( SIXTRL_NO_INCLUDES ) */
+
+#if !defined( SIXTRL_NO_INCLUDES )
+    #include "sixtracklib/testlib/common/cobjects/cobj_no_ptrs.hpp"
+    #include "sixtracklib/testlib/common/cobjects/cobj_dataptrs.hpp"
+#endif /* !defined( SIXTRL_NO_INCLUDES ) */
+
+#endif /* SIXTRACKLIB_TESTLIB_TESTLIB_CXX_HPP__ */
