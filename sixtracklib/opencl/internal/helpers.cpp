ead79b4a821f59288df3a525be5f2e3a34081f2f
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2021-03-04 10:43:32 +0100

    opencl: adds helpers for error handling and exception throwing

--- sixtracklib/opencl/internal/helpers.cpp
+++ sixtracklib/opencl/internal/helpers.cpp
@@ -9,6 +9,7 @@
 
 #if !defined( SIXTRL_NO_SYSTEM_INCLUDES ) && defined( __cplusplus )
     #include <sstream>
+    #include <stdexcept>
     #include <vector>
 #endif /* !defined( SIXTRL_NO_INCLUDES )  && defined( __cplusplus ) */
 
@@ -30,6 +31,104 @@ namespace SIXTRL_CXX_NAMESPACE
         std::string const& SIXTRL_RESTRICT_REF SIXTRL_UNUSED( env_variable_name ) )
     {
         return st::size_type{ 0 };
+    }
+
+    st::status_type OpenCL_ret_value_to_stream(
+        std::ostream& SIXTRL_RESTRICT_REF output_str,
+        ::cl_int const ret_value,
+        std::string const& SIXTRL_RESTRICT_REF class_scope,
+        std::string const& SIXTRL_RESTRICT_REF method_scope,
+        std::string const& SIXTRL_RESTRICT_REF message,
+        size_type const line_number )
+    {
+        st::status_type status = st::STATUS_GENERAL_FAILURE;
+
+        if( !class_scope.empty() )
+        {
+            output_str << class_scope;
+
+            if( !method_scope.empty() )
+            {
+                output_str << "::";
+            }
+        }
+
+        if( !method_scope.empty() )
+        {
+            output_str << method_scope;
+
+            if( line_number > st::size_type{ 0 } )
+            {
+                output_str << "(line=" << line_number << ")";
+            }
+
+            output_str << ": ";
+        }
+
+        if( ret_value != CL_SUCCESS )
+        {
+            status = st::STATUS_SUCCESS;
+
+            switch( ret_value )
+            {
+                case CL_INVALID_CONTEXT: {
+                    output_str << "CL_INVALID_CONTEXT";
+                    break;
+                }
+
+                case CL_INVALID_VALUE: {
+                    output_str << "CL_INVALID_VALUE";
+                    break;
+                }
+
+                case CL_INVALID_BUFFER_SIZE: {
+                    output_str << "CL_INVALID_BUFFER_SIZE";
+                    break;
+                }
+
+                case CL_INVALID_HOST_PTR: {
+                    output_str << "CL_INVALID_HOST_PTR";
+                    break;
+                }
+
+                case CL_MEM_OBJECT_ALLOCATION_FAILURE: {
+                    output_str << "CL_MEM_OBJECT_ALLOCATION_FAILURE";
+                    break;
+                }
+
+                case CL_OUT_OF_RESOURCES: {
+                    output_str << "CL_OUT_OF_RESOURCES";
+                    break;
+                }
+
+                case CL_OUT_OF_HOST_MEMORY: {
+                    output_str << "CL_OUT_OF_HOST_MEMORY";
+                    break;
+                }
+
+                case CL_INVALID_PLATFORM: {
+                    output_str << "CL_INVALID_PLATFORM";
+                    break;
+                }
+
+                case CL_INVALID_DEVICE: {
+                    output_str << "CL_INVALID_DEVICE";
+                    break;
+                }
+
+                default:
+                {
+                    output_str << ret_value << " (unknown)";
+                }
+            };
+        }
+
+        if( ( status == st::STATUS_SUCCESS ) && ( !message.empty() ) )
+        {
+            output_str << "\r\n" << "-> " << message;
+        }
+
+        return status;
     }
 }
 #endif /* !defined( _GPUCODE ) && defined( __cplusplus ) */
--- sixtracklib/opencl/internal/helpers.h
+++ sixtracklib/opencl/internal/helpers.h
@@ -13,6 +13,7 @@
 
 #if !defined( SIXTRL_NO_INCLUDES ) && defined( __cplusplus )
     #include <vector>
+    #include <sstream>
     #include <string>
 #endif /* !defined( SIXTRL_NO_INCLUDES ) && defined( __cplusplus ) */
 
@@ -20,11 +21,11 @@
 extern "C" {
 #endif /* !defined( _GPUCODE ) && defined( __cplusplus ) */
 
-SIXTRL_EXTERN SIXTRL_HOST_FN NS(size_type) NS(OpenCL_num_all_nodes)( void );
+// SIXTRL_EXTERN SIXTRL_HOST_FN NS(size_type) NS(OpenCL_num_all_nodes)( void );
 
-SIXTRL_EXTERN SIXTRL_HOST_FN NS(size_type) NS(OpenCL_num_available_nodes)(
-    char const* SIXTRL_RESTRICT filter_str,
-    char const* SIXTRL_RESTRICT env_variable_name );
+// SIXTRL_EXTERN SIXTRL_HOST_FN NS(size_type) NS(OpenCL_num_available_nodes)(
+//     char const* SIXTRL_RESTRICT filter_str,
+//     char const* SIXTRL_RESTRICT env_variable_name );
 
 #if !defined( _GPUCODE ) && defined( __cplusplus )
 }
@@ -39,6 +40,36 @@ namespace SIXTRL_CXX_NAMESPACE
         std::string const& SIXTRL_RESTRICT_REF filter_str = std::string{},
         std::string const& SIXTRL_RESTRICT_REF env_variable_name = std::string{}
     );
+
+    status_type OpenCL_ret_value_to_stream(
+        std::ostream& SIXTRL_RESTRICT_REF output_stream,
+        ::cl_int const ret_value,
+        std::string const& SIXTRL_RESTRICT_REF class_scope = std::string{},
+        std::string const& SIXTRL_RESTRICT_REF method_scope = std::string{},
+        std::string const& SIXTRL_RESTRICT_REF message = std::string{},
+        size_type const line_number = size_type{ 0 } );
+
+
+    template< class ExceptionT >
+    void OpenCL_ret_value_to_exception( ::cl_int const ret_value,
+        std::string const& SIXTRL_RESTRICT_REF class_scope = std::string{},
+        std::string const& SIXTRL_RESTRICT_REF method_scope = std::string{},
+        std::string const& SIXTRL_RESTRICT_REF message = std::string{},
+        size_type const line_number = size_type{ 0 } )
+    {
+        std::ostringstream a2str;
+
+        if( SIXTRL_CXX_NAMESPACE::STATUS_SUCCESS !=
+            SIXTRL_CXX_NAMESPACE::OpenCL_ret_value_to_stream(
+                a2str, ret_value, class_scope, method_scope, message,
+                    line_number ) )
+        {
+            a2str.str( "" );
+            a2str << "Unknown exception for return value ret = " << ret_value;
+        }
+
+        throw ExceptionT( a2str.str() );
+    }
 
 } /* end: namespace SIXTRL_CXX_NAMESPACE */
 #endif /* !defined( _GPUCODE ) && defined( __cplusplus ) */
