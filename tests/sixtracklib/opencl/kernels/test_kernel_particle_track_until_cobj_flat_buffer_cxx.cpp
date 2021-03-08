daf7c1c11dd02736bf5fe149b9683139d84560a7
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2021-03-08 12:25:31 +0100

    tests/opencl: add program config store support to the kernel unit tests

--- tests/sixtracklib/opencl/kernels/test_kernel_particle_track_until_cobj_flat_buffer_cxx.cpp
+++ tests/sixtracklib/opencl/kernels/test_kernel_particle_track_until_cobj_flat_buffer_cxx.cpp
@@ -15,19 +15,23 @@ TEST( CXXOpenCLKernelsParticleTrackUntilCObFlatBuffer, RunTimeCompileKernel )
 {
     namespace st = SIXTRL_CXX_NAMESPACE;
     using ctx_type = st::OclContext;
+    using prog_key_type = st::OclProgramKey;
     using program_item_type = st::OclRtcProgramItem;
 
-    std::string const program_name = "particle_track_until_cobj_flat_buffer.cl";
     std::ostringstream a2str;
+    a2str << ::NS(PATH_TO_BASE_DIR) << "tests/sixtracklib/opencl/control/"
+          << "opencl.toml";
+
+    st::OclProgramConfigStore program_configs;
+    program_configs.update_from_file( a2str.str() );
+    a2str.str( "" );
+
+    std::string const program_name = "particle_track_until_cobj_flat_buffer.cl";
     a2str << ::NS(PATH_TO_BASE_DIR) << "sixtracklib/opencl/kernels/"
           << program_name;
     std::string const path_to_kernel_src = a2str.str();
     a2str.str( "" );
 
-    a2str << "-I" << ::NS(PATH_TO_SIXTRL_INCLUDE_DIR) << " -Werror";
-    std::string const compile_options = a2str.str();
-
-    a2str.str( "" );
     a2str << SIXTRL_C99_NAMESPACE_PREFIX_STR
           << "Track_until_particle_cobj_flat_buffer_opencl";
     std::string const kernel_name = a2str.str();
@@ -48,12 +52,19 @@ TEST( CXXOpenCLKernelsParticleTrackUntilCObFlatBuffer, RunTimeCompileKernel )
         ASSERT_TRUE( ctx->key().num_devices == st::size_type{ 1 } );
         ASSERT_TRUE( ctx->key().contains_node_id( node_id ) );
 
+        auto config = program_configs.find( prog_key_type{
+            ctx->key(), program_name } );
+        ASSERT_TRUE( config != nullptr );
+        a2str << *config;
+
         program_item_type program_from_file;
 
         ASSERT_TRUE( program_from_file.create_from_source_file(
             *ctx, path_to_kernel_src ) == st::STATUS_SUCCESS );
 
-        program_from_file.set_compile_flags( compile_options );
+        program_from_file.set_compile_flags( a2str.str() );
+        a2str.str( "" );
+
         ASSERT_TRUE( st::STATUS_SUCCESS == program_from_file.build(
             *ctx, program_name ) );
 
